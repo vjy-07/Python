d574421c65da3ea50eb1f6667c38619ee0762008
vjy-07 <gvijaycrl@gmail.com>
2025-11-28 01:04:14 +0530

    strings

---
+++ Part 3/29_strings.py
@@ -0,0 +1,17 @@
+#1
+word = "python"
+print(len(word))
+
+#2
+word1 = "abc"
+word2 = "def"
+res = word1+word2
+print(res)
+
+#3
+temp = "test"
+print(temp[2])
+for ch in temp:
+    print(ch)
+
+#strings are immutable
