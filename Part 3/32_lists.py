93e703d32463e02995cc49c028913f3ba0f29807
vjy-07 <gvijaycrl@gmail.com>
2025-11-29 21:41:59 +0530

    add lists

---
+++ Part 3/32_lists.py
@@ -0,0 +1,15 @@
+#1
+marks = [30,31,32,33,34]
+print(marks[2])
+
+#2
+marks[1]=40
+print(marks)
+
+#3
+list1 = [23,2,0.33,'c',"abcde"]
+print(list1)
+print(type(list1))
+
+#4
+print(marks[2:len(list1)])
