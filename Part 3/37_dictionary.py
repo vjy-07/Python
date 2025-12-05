80c325921a0e17c5f4c454cb5bc14fb396a44e96
vjy-07 <gvijaycrl@gmail.com>
2025-12-05 06:59:14 +0530

    add dictionary

---
+++ Part 3/37_dictionary.py
@@ -0,0 +1,12 @@
+info = {
+    "name": "vijay",
+    "sub":["math","phy","che"],
+    "rollNo": 123
+}
+print(info)
+print(type(info))
+
+print(info["sub"])
+
+info["rollNo"]=345
+print(info["rollNo"])
