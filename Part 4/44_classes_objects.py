53a365f2d388bfb6d087c36c938d37cb4a51e0fe
vjy-07 <gvijaycrl@gmail.com>
2025-12-08 17:38:20 +0530

    add classes and objects

---
+++ Part 4/44_classes_objects.py
@@ -0,0 +1,9 @@
+class Student:
+    subject = "Python"
+    college = "ABC"
+    year = "4th year"
+
+stu1 = Student()
+stu2 = Student()
+print(stu1.subject, stu1.college, stu1.year)
+print(stu2.subject, stu2.college, stu2.year)
